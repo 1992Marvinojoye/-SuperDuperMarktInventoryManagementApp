257f216a600d603d3a670983e8d20e8a9365fa2a
Marvin Ojoye <marvin92ojoye@gmail.com>
2021-06-01 19:40:47 +0200

    initial strukture
    basic Package Struckture of the Project

---
+++ src/main/java/SuperDuperMarkt/Inventory/Management/App/MainApp.java
@@ -0,0 +1,21 @@
+package SuperDuperMarkt.Inventory.Management.App;
+
+/**
+ * 
+ */
+
+/**
+ * @author marvin_ojoye
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
